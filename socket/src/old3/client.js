4df885f05f94b94f5e8bd5873967a239a82aa170
Christian Passold <passold.christian@hotmail.com>
2018-09-27 23:35:55 -0300

    wip

---
+++ socket/src/old3/client.js
@@ -0,0 +1,93 @@
+const io = require('socket.io-client')
+const commands = require('./command.js')
+const utils = require('../util.js')
+const ioClient = io.connect("http://localhost:8000")
+
+let _isBeingConsumed = false
+const waiting = [] //push and shift
+let _isWaitingResponse = false
+//timeouts
+let tmoConsumeCoordinator = null
+let tmoConsumingResource = null
+
+let _id = null
+let _socketId = null
+
+ioClient.on('updateId', (id, socketId) => {
+    _socketId = socketId
+    _id = id
+    console.log('ID updated => ' + _id)
+})
+
+ioClient.on('kill', () => {
+    waiting.forEach(process => ioClient.emit('releaseProcess', process.socketId))
+    ioClient.disconnect()
+});
+
+ioClient.on('command', (command, who) => {
+    switch (command) {
+        //only coordinator
+        case commands.CONSUME : {
+            requestConsume(who)
+            break
+        }
+
+        //all
+        case commands.RELEASE: {
+            releaseProcess()
+            break
+        }
+        default:
+            console.log('command not found => ' + command)
+    }
+})
+
+//client
+function releaseProcess() {
+    console.log("releasing : " + _id)
+    _isWaitingResponse = false
+}
+
+function requestServerConsume() {
+    if (_isWaitingResponse) return
+
+    _isWaitingResponse = true
+    ioClient.emit('requestConsume', _socketId)
+
+    tmoConsumeCoordinator = setTimeout(() => requestServerConsume(), (utils.randomInRange(10, 25) * 1000))
+}
+
+//coordinator
+function requestConsume(who) {
+    if (!who) return
+
+    waiting.push(who)
+
+    console.log(_isBeingConsumed)
+    if (_isBeingConsumed) {
+        console.info(`Client ${who.id} is waiting...`)
+    } else {
+        consume(waiting.shift())
+    }
+
+}
+
+function consume(who) {
+    _isBeingConsumed = true
+    console.info(`Client ${who.id} is consuming`)
+    tmoConsumingResource = setTimeout(() => releaseResource(who.socketId), utils.randomInRange(5, 15))
+}
+
+function releaseResource(socketId) {
+    ioClient.emit('releaseProcess', socketId)
+    if (waiting.length > 0) {
+        consume(waiting.shift())
+    } else {
+        _isBeingConsumed = false
+    }
+}
+
+
+//end
+
+requestServerConsume()
--- socket/src/server.js
+++ socket/src/server.js
@@ -1,60 +1,30 @@
 const io = require('socket.io')
-const commands = require('./command.js')
-const utils = require('./util.js')
 
 const server = io.listen(8000)
 
-const _sockets = new Map() //socket id -> {socket, id}
-let _coordinator = null
-let _latestIdClient = 0
+let clients = new Map()
+let latestIdClient = 0
+let coordinator = null
+let isElecting = false
 
 // event fired every time a new client connects:
 server.on('connection', (socket) => {
-    const id = ++_latestIdClient
+
+    const id = ++latestIdClient
+
+    socket.emit("update",id)
+
+
+    // initialize this client's sequence number
+    clients.set(id, socket)
 
     console.info(`Client connected [id=${id}]`)
-    _sockets.set(socket.id, {socket, id})
-
-    socket.emit('updateId', id, socket.id)
-
-    socket.on('releaseProcess', () => {
-        socket.emit('command', commands.RELEASE)
-    })
-
-    socket.on('requestConsume', (socketId) => processConsumeCoordinator(_sockets.get(socketId)))
 
     // when socket disconnects, remove it from the list:
     socket.on('disconnect', () => {
-        console.info(`Client gone [id=${_sockets.get(socket.id).id}]`)
-        _sockets.delete(socket.id)
-        if (socket === _coordinator) {
-            _coordinator = null
-        }
-    })
-})
-
-function processConsumeCoordinator(process) {
-    if (!_coordinator) {
-        _coordinator = utils.electNewCoordinator(_sockets)
-        if (!!_coordinator)
-            console.info("new coordinator id " + _sockets.get(_coordinator.id).id)
-    } else {
-        if (!!process && process.socket !== _coordinator) {
-            _coordinator.emit("command", commands.CONSUME,
-                {id: process.id, socketId: process.socket.id})
-        }
-    }
-}
-
-// Kill coordinator
-setInterval(() => {
-    if (!!_coordinator) {
-        console.info(`Coordinator [id=${_sockets.get(_coordinator.id).id}] killed`)
-        _sockets.delete(_coordinator.id)
-        _coordinator.emit('kill')
-        _coordinator = null
-    }
-}, 60 * 1000)
-
+        clients.delete(socket)
+        console.info(`Client gone [id=${id}]`)
+    });
+});
 
 console.info("Waiting clients...")
